00e8dc8b9c90d93c07006c2f8ed03f8b36661945
showboat051 <rashad.davis87@yahoo.com>
2018-07-22 02:43:24 -0500

    fully working, needs styles

--- reactweather/src/App.js
+++ reactweather/src/App.js
@@ -6,30 +6,67 @@ import Weather from './components/Weather';
 
 
 class App extends Component {
+  
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined,
+  };
 
-async componentDidMount(){
-  console.log("MOUNTED")
-}
-getWeather =  async (e)=>{ 
+  getWeather =  async (e)=>{ 
         
     e.preventDefault();
-    const api_key = "gYKvBB0KdGA3YlavbAuaenogPATVUAl6";
-    console.log("trying to get weather!")
-  const api_call = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${api_key}`)
+    const city = e.target.elements.city.value;
+    const state = e.target.elements.state.value;
+
+    const api_key = "&APPID=ff05166f3c7814d58199e22969528af4";
+  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${state}${api_key}`);
   const data = await api_call.json();
-  console.log (data);
+  
+  if ( city) {
+    console.log (data);
+    this.setState({
+    temperature: data.main.temp,
+    city: data.name,
+    country: data.sys.country,
+    humidity: data.main.humidity,
+    description: data.weather[0].description,
+    error: ""
+  });
+} else {
+    console.log (data);
+    this.setState({
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: "Please enter the value."
+  });
 }
+  
+  }
 
   render() {
     return (
       <div>
         <Topheader />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather temperature ={this.state.temperature}
+          city = {this.state.city}
+          country = {this.state.country}
+          humidity = {this.state.humidity}
+          description = {this.state.description}
+          error = {this.state.error}
+        />
         <button onClick={this.test}>TEST</button>
       </div>
-    );
+  );
   }
 }
+
 
 export default App;
--- reactweather/src/components/Form.js
+++ reactweather/src/components/Form.js
@@ -6,7 +6,7 @@ class Form extends React.Component {
         return(
             <form onSubmit={this.props.getWeather}>
                 <input type ="text" name="city" placeholder="City"/>
-                <input type ="text" name="country" placeholder="Country"/>
+                <input type ="text" name="state" placeholder="State"/>
                 <button >Get Weather</button>
 
             </form>
